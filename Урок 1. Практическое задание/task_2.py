8a2fc16090f67db6c0aa7a02819194895f4c6feb
shpil97 <89089556+shpil97@users.noreply.github.com>
2022-08-22 14:24:10 +0300

    Update Урок 1. Практическое задание/task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,30 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+
+from random import randint
+
+
+def lst_min_1(lst):  # O(n ** 2)
+    for i in lst:
+        is_min = True
+        for j in lst:
+            if i > j:
+                is_min = False
+        if is_min:
+            return i
+
+
+def lst_min_2(lst):   # O(N)
+    min_num = lst[0]
+    for i in lst:
+        if i < min_num:
+            min_num = i
+    return min_num
+
+
+lst1 = [randint(0, 100) for i in range(20)]
+print(lst1)
+print(lst_min_1(lst1))
+print(lst_min_2(lst1))
